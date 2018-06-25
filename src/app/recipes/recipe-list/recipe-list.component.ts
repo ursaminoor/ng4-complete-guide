06c7ada1bf10dfdc6d3cb940f1122b0492d7b596
Darya <dasha.grishanovich@gmail.com>
2018-06-25 16:38:54 +0300

    added recipe details and ingredients

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test recipe', 'This is simply a test',
+     'https://www.skinnytaste.com/wp-content/uploads/2017/08/Grilled-Garlic-and-Herb-Chicken-and-Veggies-1-2.jpg'),
+    new Recipe('Recipe 2', 'Another one recipe', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2015/11/20/3/FNK_Kids-Can-Make-OPENER_s4x3.jpg.rend.hgtvcom.966.725.suffix/1448050653882.jpeg')
+  ];
 
   constructor() { }
 
